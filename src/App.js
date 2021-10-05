fe5f5df2d6a04084880a70d21a45c8dd46b5980d
MichaelAbe <michael.k.abe@gmail.com>
2021-10-05 12:18:22 -0400

    created components/metronome.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Metronome from './Components/Metronome';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <Metronome/>
     </div>
   );
 }
