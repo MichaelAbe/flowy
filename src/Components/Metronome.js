6311fab5813f30cdfdfe52d7b7863d2f4ea7beaf
MichaelAbe <michael.k.abe@gmail.com>
2021-10-21 16:04:42 -0400

    created time component

--- src/Components/Metronome.js
+++ src/Components/Metronome.js
@@ -5,7 +5,8 @@ export default function Metronome() {
   const [bpm, setBpm] = useState(120)
   let [beat, setBeat] = useState(0)
   let [timeSignature, setTimeSignature] = useState(4)
-  let [tempo, setTempo] = useState(bpm);
+  let [tempo, setTempo] = useState(bpm)
+  const [count, setCount] = useState(0)
   const [playing, setPlaying] = useState(false)
   const clickBase = new Audio(
     "http://www.denhaku.com/r_box/sr16/sr16perc/hi%20block.wav"
@@ -14,18 +15,41 @@ export default function Metronome() {
     "http://drbraukmann.com/DESN275/soundLibrary/toolsBangs/tapOnWood.WAV"
   );
 
-  useEffect(() => {
-    spaceBar()
-  }, []);
+  // useEffect(() => {
+  //   spaceBar()
+  //   aTest()
+  //   console.log('Hello')
+  // }, []);
 
-  const spaceBar = () => {
-    document.addEventListener('keydown', (e) => {
-      if (e.key == ' ') {
-        handlePlay() 
-        console.log("hello")
-      }
-    }) 
-  }
+  // const tTest = () => {
+  //   document.addEventListener('keyup', (e) => {
+  //     e.preventDefault
+  //     if (e.key == 't') {
+  //       handleBpmPlus()
+  //     }
+  //   })
+  // }
+
+  // const spaceBar = () => {
+  //   document.addEventListener('keyup', (e) => {
+  //     e.preventDefault()
+  //     if (e.key == ' ') {
+  //       handlePlay() 
+  //       setPlaying(!playing)
+  //       setBpm(bpm + 10)
+  //     }
+  //   }) 
+  // }
+
+  // const aTest = () => {
+  //   document.addEventListener('keydown', (e) => {
+  //     e.preventDefault()
+  //     if (e.key == 'a') {
+  //       setBpm(bpm + 10)
+  //       console.log(bpm)
+  //     }
+  //   })
+  // }
 
   const handleBpmChange = (e) => {
     e.preventDefault()
@@ -34,37 +58,40 @@ export default function Metronome() {
   };
 
   const playMeasure = () => {
+  
    if(beat === 0) {
      playClickAccent()
    } else {
      playClickBase()
    }
-   setBeat(beat + 1)
-   console.log(beat)
+     setBeat(beat + 1)  
+  //  console.log(beat)
   }
 
-  // const counterTest = () => {
-  //   setBeat(beat = 1)
-  //   console.log(beat)
-  // }
   
   const handlePlay = (e) => {
+    console.log(playing)
     if (!playing) {
-      setTempo(setInterval(playMeasure, (60 / parseInt(bpm)) * 1000));
-      setPlaying(true);
-    } else {
-      clearInterval(tempo);
-      setPlaying(false);
-    }
-  };
+      setTempo(setInterval(playMeasure, (60 / parseInt(bpm)) * 1000))
+      setPlaying(true)
 
-  const handleBpmPlus = () => setBpm(parseInt(bpm) + 1)
-  
+    } else {
+      clearInterval(tempo)
+      setPlaying(false)
+    } 
+    // console.log(playing)
+  }
+
+  const handleBpmPlus = () => {
+    setBpm(parseInt(bpm) + 1)
+  }
+
   const handleBpmMinus = () => setBpm(parseInt(bpm) - 1)
 
   const playClickBase = () => {
     clickBase.play()
-  };
+    
+  }
 
   const playClickAccent = () => {
     clickAccent.play()
