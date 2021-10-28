c4af215c55ff158fe5f7ad8d65b40792db02172a
MichaelAbe <michael.k.abe@gmail.com>
2021-10-28 15:22:14 -0400

    making a key dropdowm

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Key() {
     return (
-        'key.js'
-    )
+      <div>
+        <span>
+          <select name="keyselect" id="keyselect" className="key">
+            {/*   <option value="4">4/4</option>  <option value="6">6/8</option> */}
+          </select>
+        </span>
+      </div>
+    );
 }
