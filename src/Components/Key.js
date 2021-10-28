f44b9368aa9d949efcd1316431bfb2043ffa5458
MichaelAbe <michael.k.abe@gmail.com>
2021-10-28 15:27:02 -0400

    added major keys

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -6,13 +6,12 @@ export default function Key() {
         <span>
           <select name="keyselect" id="keyselect" className="key">
             <option value="a">A</option>  
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
-            <option value="6">6/8</option>
+            <option value="b">B</option>
+            <option value="c">C</option>
+            <option value="d">D</option>
+            <option value="e">E</option>
+            <option value="f">F</option>
+            <option value="g">G</option>
           </select>
         </span>
       </div>
