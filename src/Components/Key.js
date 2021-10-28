700906cc429222a5c71af17131887662494fbb44
MichaelAbe <michael.k.abe@gmail.com>
2021-10-28 15:30:40 -0400

    added flat and sharp options

--- src/Components/Key.js
+++ src/Components/Key.js
@@ -5,13 +5,20 @@ export default function Key() {
       <div>
         <span>
           <select name="keyselect" id="keyselect" className="key">
-            <option value="a">A</option>  
-            <option value="b">B</option>
-            <option value="c">C</option>
-            <option value="d">D</option>
-            <option value="e">E</option>
-            <option value="f">F</option>
-            <option value="g">G</option>
+            <option value="A">A</option>  
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+            <option value="E">E</option>
+            <option value="F">F</option>
+            <option value="G">G</option>
+            <option value="Db">Db</option>
+            <option value="C#">C#</option>
+            <option value="Eb">Eb</option>
+            <option value="Gb">Gb</option>
+            <option value="F#">F#</option>
+            <option value="Ab">Ab</option>
+            <option value="Bb">Bb</option>
           </select>
         </span>
       </div>
