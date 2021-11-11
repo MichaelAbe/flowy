cb5a3dface96231ccbdc8551395b625b5bfb9b56
MichaelAbe <michael.k.abe@gmail.com>
2021-11-11 13:10:40 -0500

    moved event handleler to dropdown component

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function Dropdown() {
+  const [time, setTime] = useState(4)
+
+  const handleTimeChange = (e) => {
+    e.preventDefault()
+    let time = e.target.value
+    setTime(time)
+  }
+  
   return (
     <div>
         <span>
-      <select name="selectList" id="selectList" className="time">
+      <select name="selectList" id="selectList" className="time" onClick={handleTimeChange}>
           <option value="4">4/4</option> {" "}
         <option value="6">6/8</option>
       </select>
-
+        time={time}
         </span>
     </div>
   );
--- src/Components/Time.js
+++ src/Components/Time.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import Dropdown from './Dropdown'
-import { useState, useEffect } from "react"
+
 
 export default function Time() {
-    const [time, setTime] = useState(4)
 
-  const handleKeyChange = (e) => {
-    e.preventDefault()
-    let time = e.target.value
-    setTime(time)
-  }
-  
     return (
         <div>
             <span>
             <Dropdown/>
-            time={time}
+           
             </span>
         
         </div>
