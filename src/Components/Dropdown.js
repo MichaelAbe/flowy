2a5155c167ff849f6264950d381663656e69b9cc
MichaelAbe <michael.k.abe@gmail.com>
2021-11-18 11:30:10 -0500

    reorder time signatures

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -14,8 +14,8 @@ export default function Dropdown() {
     <div>
         <span>
       <select name="selectList" id="selectList" className="time" onClick={handleTimeChange}>
-        <option value="3">3/4</option>
           <option value="4">4/4</option> {" "}
+        <option value="3">3/4</option>
         <option value="6">6/8</option>
       </select>
         time={time}
