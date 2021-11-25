430042796b5e3714913d0efb4a656924a2082452
MichaelAbe <michael.k.abe@gmail.com>
2021-11-25 10:52:50 -0500

    added and rendered input component

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Input from './Input'
 
 export default function Song() {
     return (
         <div>
             This is the song component
+            <Input/>
         </div>
     )
 }
