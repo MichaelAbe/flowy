0a488bf9f17580316dfd80f3ea2079c0b5ad263c
MichaelAbe <michael.k.abe@gmail.com>
2021-12-02 12:49:02 -0500

    playlist dropdown

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -2,8 +2,32 @@ import React from 'react'
 
 export default function Playlist() {
     return (
-        <div>
-            Playlist Component
-        </div>
-    )
+      <div>
+        Add to Playlist:{" "}
+        <span>
+          <select
+            name="selectList"
+            id="selectList"
+            className="time"
+            
+          >
+              <option value="4">4/4</option>  <option value="3">3/4</option>
+            <option value="6">6/8</option>
+          </select>
+          
+        </span>
+      </div>
+    );
 }
+
+
+{/* <div>
+<span>
+<select name="selectList" id="selectList" className="time" onClick={handleTimeChange}>
+  <option value="4">4/4</option> {" "}
+<option value="3">3/4</option>
+<option value="6">6/8</option>
+</select>
+time={time}
+</span>
+</div> */}
