d28b5920a4d83d6a9304ec336da624f6455b4d7f
MichaelAbe <michael.k.abe@gmail.com>
2021-12-09 11:52:30 -0500

    imported use state

--- src/Components/Style.js
+++ src/Components/Style.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Style() {
+    
+    const [style, setStyle] = useState('')
+    
     return (
+
+
+
         <div>
             Style component 
+
         </div>
     )
 }
+
+
+
+
+
+// const [playlist, setPlaylist] = useState('')
+
+// const handePlaylistChange = (e) => {
+//     e.preventDefault()
+//     let playlist = e.target.value
+//     setPlaylist(playlist)
+// }
+
+// return (
+//   <div>
+//     Add to Playlist:{" "}
+//     <span>
+//       <select
+//         name="playList"
+//         id="playList"
+//         className="time"
+//         onClick={handePlaylistChange}
+//       >
+//           <option value="Practice">Practice</option>  
+//           <option value="Preformance">Preformance</option>
+//           <option value="Other">Other</option>
+//       </select>
+      
+//     </span>
+//     Playlist = {playlist}
+//   </div>
+// );
+// }
