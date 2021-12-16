525b0db5024c4757b30017746e4235aff3715d38
MichaelAbe <michael.k.abe@gmail.com>
2021-12-16 10:13:57 -0500

    working on blinking function

--- src/Components/Blinker.js
+++ src/Components/Blinker.js
@@ -4,12 +4,17 @@ import { useState } from 'react'
 export default function Blinker() {
     
     const [blink, useBlink] = useState(false)
-    
+    const [counter, useCounter] = useState(0)
+
+    const blinking = () => {
+        setBlink(blink + 1)
+    }
     return (
 
 
-        <div>
+        <div className='blinker'>
             Blinker
+            {counter}
         </div>
     )
 }
